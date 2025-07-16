c0ff27ef791d23e352eac27a73c997fac64449cc
João Marcelo <joaovictormarcelo5@gmail.com>
2025-07-16 09:38:59 -0300

    separando as responsabilidades dos handler e interfaces

---
+++ TodoList.Proj/Handlers/PostHandler/TaskhandlerCreate.cs
@@ -0,0 +1,14 @@
+﻿using TodoList.Proj.Models;
+using TodoListCore.ControllersHandlers;
+using TodoListCore.Response;
+using ViewModels.Todo;
+
+namespace TodoList.Proj.Handlers.PostHandler;
+
+public class TaskhandlerCreate: ITaskHandlerCreate
+{
+    public Task<Responses<Todo?>> CreateAsync(TodoDTO request)
+    {
+        throw new NotImplementedException();
+    }
+}

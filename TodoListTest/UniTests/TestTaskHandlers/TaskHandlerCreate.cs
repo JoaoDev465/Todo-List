1de6365d3be45246be0a179473806c7f678938dc
João Marcelo <joaovictormarcelo5@gmail.com>
2025-07-22 09:52:53 -0300

    adicionando teste unitario de TAskHandlerCreate

---
+++ TodoListTest/UniTests/TestTaskHandlers/TaskHandlerCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using TodoList.Proj.Data;
+using TodoList.Proj.Handlers.PostHandler;
+using TodoListCore.ControllersHandlers;
+using ViewModels.Todo;
+using Xunit;
+using Assert = Xunit.Assert;
+
+namespace TodoListTest.MockTests.TestTaskHandlers;
+
+public class TaskHandlerCreate
+{
+
+    [Fact]
+    public async Task TestTaskCreate_When_Content_Is_Created()
+    {
+        var options = new DbContextOptionsBuilder<Context>()
+            .UseInMemoryDatabase(databaseName: "TODO")
+            .Options;
+
+        var context = new Context(options);
+
+        var handler = new TaskhandlerCreate(context);
+        var request = new TodoDTO
+        {
+            Task = "Ir ao Supermercado"
+        };
+
+        var result = await handler.CreateAsync(request);
+
+        Assert.NotNull(result);
+        Assert.Equal("Ir ao Supermercado",request.Task);
+    }
+
+}
---
+++ TodoListTest/UniTests/TestTaskHandlers/TestTAskGetHandler.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace TodoListTest.MockTests.TestTaskHandlers;
+
+public class TestTAskGetHandler
+{
+    [Fact]
+    public async Task Test_IsValid_When_ReturnGetContent()
+    {
+        
+    }
+}
